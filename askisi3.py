50b3c93a26feaf220613fff384eb3b98d6e60d10
kostas-simi <ksimirio@gmail.com>
2019-01-09 22:24:45 +0200

    noComm-> I think code is optimized.Seems to work under all scenarios.

--- askisi3.py
+++ askisi3.py
@@ -1,25 +1,26 @@
-#! Python program that remove comments (with #) for a python code.
+#! Python program that removes comments (with #) for a python code.
 #! Created by Kostas Simiriotis, January 2019 for the Python test of the 1st semester.
 
-print("Welcome to python comment remover, noComm1.0 by Kostas Simiriotis!")
-filename = input("Please provide the location of the file: ")
-name = filename.split(".py")
+print("Welcome to Python comment remover, noComm1.0 by Kostas Simiriotis!")
+filename = input("Please provide the name of the file: ")
 print("Opening the file...")
 finput = open(filename, 'r')
 
-foutput = open(name[0]+'_noComm'+'.py', 'w')
+foutput = open('noComm_'+filename, 'w')
 print("Removing comments...")
 for line in finput:
-    wholeline = False
+    hashInPrint = False
     l = line.split("#")
-    if len(l)>1 and ('"' in l[0]) and ('"' in l[1]):
-        foutput.write(line)
-        wholeline = True
-    if len(l) > 1 and not wholeline and l[0] != "":
+    #The following 'if' is added for the cases of print("#not a comment") or print(" student #P18135")
+    if len(l)>1 and ('(' in l[0]) and (')' in l[1]):
+        foutput.write(l[0]+"#"+l[1]+"\n")
+        hashInPrint = True
+    if len(l) > 1 and not hashInPrint and l[0] != "":
         l[0] += "\n"
 
-    if (l[0] != "") and not wholeline:
+    if (l[0] != "") and not hashInPrint:
         foutput.write(l[0])
 finput.close()
-
+print('''New file can be found under the name:\n
+            noComm_'''+filename)
 foutput.close()
