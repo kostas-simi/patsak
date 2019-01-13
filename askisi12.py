9ddc99b4738fcf9558a112d986772549c2453aed
KOSTAS <ksimirio@gmail.com>
2019-01-13 19:04:35 +0200

    ex12-> Might be near the end..

--- askisi12.py
+++ askisi12.py
@@ -4,12 +4,32 @@
 import string
 
 AB = [['' for i in range(2)] for j in range(26)]
+
 for i in range(26):
     AB[i][0] = list(string.ascii_uppercase)[i]
     AB[i][1] = 0
-
 text = input("Please type the text. ")
+# text = 'Python program that makes the most popular letters the least and vice versa.'
 text = text.upper()
 
 for i in range(26):
     AB[i][1] = text.count(AB[i][0])
+
+
+for i in range(26):
+    if (AB[i][1] == 0):
+        AB[i].pop(0)
+inText = [x for x in AB if x != [0]]
+AB = [x for x in AB if x != [0]]
+
+print(inText)
+
+inText = sorted(inText, key=lambda x: x[1])
+AB = sorted(inText, key=lambda x: x[1], reverse=True)
+
+print(inText)
+print(AB)
+counter = len(inText) -1
+for i in range(len(inText)):
+    text = text.replace(inText[i][0],AB[i][0])
+print(text)
